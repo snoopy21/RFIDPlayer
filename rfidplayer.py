8339d4ed3488096b87e27547dc5463ad49a887b4
martin <martin@vilan.ch>
2020-01-02 20:29:30 +0000

    alle Fehler abfangen, dieselbe UID nicht immer wieder starten

--- rfidplayer.py
+++ rfidplayer.py
@@ -3,6 +3,7 @@ based on example_get_uid.py
 reads uid an looks for an existing configuration
 else it plays RadioBob
 """
+import sys
 import RPi.GPIO as GPIO
 
 from pathlib import Path
@@ -19,6 +20,7 @@ if __name__ == '__main__':
         print('Waiting for RFID/NFC card...')
         base_path = (Path(__file__).parent / "../nfc_uids")
         nfc_uid_string = ''
+        active_uid = ''
         for_counter = 0
         while True:
             # Check if a card is available to read
@@ -35,15 +37,23 @@ if __name__ == '__main__':
                     nfc_uid_string += '_' + hex(i)
                 for_counter += 1
             print('UID-String:', nfc_uid_string)
-            # try open file
-            try:
-                nfc_uid_path = (base_path / nfc_uid_string).resolve()
-                #print(nfc_uid_path)
-                with open(nfc_uid_path) as f:
-                    print(f.read().rstrip())
-                f.close()
-            except IOError as e:
-                print('IOError', e)
+            # active uid does not need a restart
+            if nfc_uid_string != active_uid:
+                # try open file
+                try:
+                    nfc_uid_path = (base_path / nfc_uid_string).resolve()
+                    #print(nfc_uid_path)
+                    with open(nfc_uid_path) as f:
+                        print(f.read().rstrip())
+                    f.close()
+                    # remember actual uid
+                    active_uid = nfc_uid_string
+                except IOError as e:
+                    print('IOError', e)
+                except:
+                    print('was ging gründlich schief:', sys.exc_info()[0])
+            else:
+                print('läuft bereits')
             for_counter = 0
             nfc_uid_string = ''
     except Exception as e:
